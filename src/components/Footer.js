70f638caabc7bea74da44afbdb5338011ba34268
Naiad Baksh <NaiadBaksh@Naiads-MacBook-Pro.local>
2021-03-29 20:17:24 -0400

    Added social buttons Added line delineating footer Added relevant styling (increased to 1.5 rem on .footer-container padding top

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,29 +6,7 @@ import {Link} from 'react-router-dom';
 function Footer() {
     return (
         <div className='footer-container'>
-            {/* <section className="footer-subscription">
-                <p className="footer-subscription-heading">
-                    Sign up to hear more about me.
-                </p>
-                <p className="footer-subscription-text">
-                    You can unsubscribe at any time.
-                </p>
-                <form>
-                    <input type="email" name='email' className="footer-input" placeholder='Your Email'/>
-                </form>
-                <Button buttonStyle='btn--outline'>Submit</Button>
-            </section> */}
                 <div className="footer-links">
-                    <div className="footer-link-wrapper">
-                        <div className="footer-link-items">
-                           {/* <h2>About</h2>  */}
-                           <Link to='/about'>About</Link>
-                        </div>
-                        <div className="footer-link-items">
-                           {/* <h2>Contact</h2>  */}
-                           <Link to='/sign-up'>Contact</Link>
-                        </div>
-                    </div>
                     <div className="footer-link-wrapper">
                         <div className="footer-link-items">
                            <a href='https://www.linkedin.com/in/naiad-baksh-91963aa0/' target='_blank' rel='noopener noreferrer'> 
@@ -59,7 +37,7 @@ function Footer() {
                 <section className="social-media">
                     <div className="social-media-wrap">
                         <Link className="social-logo">
-                            NAIAD <i class="nb-logo"></i>
+                            NAIAD <i class="nb-logo-blue"></i>
                         </Link>
                     </div>
                     <small className="website-rights">Naiad Baksh © 2021</small>
