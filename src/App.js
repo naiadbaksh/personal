536be4614dfdf929c207abbad4025c9a08f513b5
Naiad Baksh <65573342+naiadbaksh@users.noreply.github.com>
2026-04-21 22:53:48 -0400

    Change Router to HashRouter and update structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "./components/navbar/Navbar";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
@@ -13,40 +13,35 @@ import ThoughtGardenProject from "./components/pages/ThoughtGardenProject";
 
 function App() {
   return (
-    <>
-      <Router>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/about" exact component={About} />
-          <Route path="/contact" exact component={Contact} />
-          <Route
-            path="/pathfindingvisualizer"
-            exact
-            component={PathfindingProject}
-            id="pathfinder"
-          />
-          <Route
-            path="/ecommerce"
-            exact
-            component={eCommerceProject}
-            id="ecommerce"
-          />
-          <Route
-            path="/aichatbot"
-            exact
-            component={ChatbotProject}
-            id="aichatbot"
-          />
-          <Route
-            path="/thoughtgarden"
-            exact
-            component={ThoughtGardenProject}
-            id="thoughtgarden"
-          />
-          <ChatBot />
-        </Switch>
-      </Router>
-    </>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" exact component={About} />
+        <Route path="/contact" exact component={Contact} />
+        <Route
+          path="/pathfindingvisualizer"
+          exact
+          component={PathfindingProject}
+        />
+        <Route
+          path="/ecommerce"
+          exact
+          component={eCommerceProject}
+        />
+        <Route
+          path="/aichatbot"
+          exact
+          component={ChatbotProject}
+        />
+        <Route
+          path="/thoughtgarden"
+          exact
+          component={ThoughtGardenProject}
+        />
+      </Switch>
+      <ChatBot />
+    </Router>
   );
 }
 
